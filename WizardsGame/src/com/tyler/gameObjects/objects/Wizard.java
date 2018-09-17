c4e2acb5d72f3a403f386fcf22799f67f4fff6ed
TyGuy464646 <t.eldred4646@gmail.com>
2018-09-16 20:45:10 -0600

    added basic collision

--- WizardsGame/src/com/tyler/gameObjects/objects/Wizard.java
+++ WizardsGame/src/com/tyler/gameObjects/objects/Wizard.java
@@ -24,17 +24,20 @@ public class Wizard extends GameObject {
         x += velX;
         y += velY;
 
+        collision();
+
+        // movement
         if(handler.isUp()) velY = -5;
-        else if (!handler.isDown()) velY = 0;
+        else if(!handler.isDown()) velY = 0;
 
         if(handler.isDown()) velY = 5;
-        else if (!handler.isUp()) velY = 0;
+        else if(!handler.isUp()) velY = 0;
 
         if(handler.isRight()) velX = 5;
-        else if (!handler.isLeft()) velX = 0;
+        else if(!handler.isLeft()) velX = 0;
 
         if(handler.isLeft()) velX = -5;
-        else if (!handler.isRight()) velX = 0;
+        else if(!handler.isRight()) velX = 0;
     }
 
     public void render(Graphics g) {
@@ -44,5 +47,21 @@ public class Wizard extends GameObject {
 
     public Rectangle getBounds() {
         return new Rectangle(x, y, 32, 48);
+    }
+
+
+    // METHODS
+    private void collision() {
+        for(int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+
+            if(tempObject.getId() == ID.Block) {
+                if(getBounds().intersects(tempObject.getBounds())) {
+                    x += velX * -1;
+                    y += velY * -1;
+
+                }
+            }
+        }
     }
 }
