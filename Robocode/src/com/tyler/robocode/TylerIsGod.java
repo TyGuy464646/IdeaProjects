931a104da53c743404631dbead2ddf1a10177a70
TyGuy464646 <t.eldred4646@gmail.com>
2018-10-03 15:51:12 -0600

    Changed TylerIsGod Robot

--- Robocode/src/com/tyler/robocode/TylerIsGod.java
+++ Robocode/src/com/tyler/robocode/TylerIsGod.java
@@ -1,5 +1,37 @@
 package com.tyler.robocode;
 
-public class TylerIsGod {
-    
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+import java.awt.*;
+
+public class TylerIsGod extends Robot {
+
+    public void run() {
+        this.setBodyColor(new Color(0, 200, 0));
+        this.setGunColor(new Color(0, 150, 50));
+        this.setRadarColor(new Color(0, 100, 100));
+        this.setBulletColor(new Color(255, 255, 100));
+        this.setScanColor(new Color(255, 200, 200));
+
+
+        while (true) {
+            turnRight(90);
+        }
+    }
+
+    public void onHitWall(HitWallEvent e) {
+
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+
+    }
+
+    public void onHitRobot(HitRobotEvent e) {
+
+    }
+
 }
