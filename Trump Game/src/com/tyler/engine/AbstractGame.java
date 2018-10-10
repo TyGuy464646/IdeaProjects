39e5fb3889099dc62d824bb0dc316f2a85e83111
TyGuy464646 <t.eldred4646@gmail.com>
2018-10-10 08:59:49 -0600

    Changes

--- Trump Game/src/com/tyler/engine/AbstractGame.java
+++
@@ -1,9 +0,0 @@
-package com.tyler.engine;
-
-public abstract class AbstractGame {
-
-	public abstract void init(GameContainer gc);
-	public abstract void update(GameContainer gc, float deltaTime);
-	public abstract void render(GameContainer gc, Renderer r);
-	
-}
