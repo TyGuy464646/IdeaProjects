6fc2d871f9f5d7ebd4c30e9b95b4407c03de4bd0
TyGuy464646 <t.eldred4646@gmail.com>
2019-05-13 19:27:11 -0600

    Minor Fixes

--- TrumpGame/src/com/tyler/main/Camera.java
+++ TrumpGame/src/com/tyler/main/Camera.java
@@ -5,14 +5,15 @@ import com.tyler.gameObjects.GameObject;
 public class Camera {
 
     // Import Classes
-    Game game;
+    private Game game;
 
     // Variables
     public float x = 0,
             y = 0;
     public double vX = 0,
             vY = 0;
-    public double speed = 2;
+    public double speedX = 2;
+    public double speedY = 1;
 
 
     // Constructor
@@ -26,10 +27,11 @@ public class Camera {
         double targetY = object.getY() - (game.getScreenHeight() / 2.0);
         double distance = Math.sqrt(((targetX - x) * (targetX - x)) + ((targetY - y) * (targetY - y)));
 
-        if (distance >= 20) {
-            speed = (distance - 20);
-            vX = ((targetX - x) / distance) * speed;
-            vY = ((targetY - y) / distance) * speed;
+        if (distance >= 5) {
+            speedX = (distance - 5);
+            speedY = (distance - 5);
+            vX = ((targetX - x) / distance) * speedX;
+            vY = ((targetY - y) / distance) * speedY;
         } else {
             vX = 0;
             vY = 0;
@@ -37,5 +39,8 @@ public class Camera {
 
         x += vX;
         y += vY;
+
+//        x = object.getX() - (game.getScreenWidth() / 2);
+//        y = object.getY() - (game.getScreenHeight() / 2);
     }
 }
